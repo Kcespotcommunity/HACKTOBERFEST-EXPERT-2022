fd041f7e332589ccde95cfc185d567c8d191f388
KishoreMSK <mkishore2610@gmail.com>
2022-10-03 21:21:28 +0530

    Router Setup, Dashboard and wildcard

--- gym-frontend/src/App.vue
+++ gym-frontend/src/App.vue
@@ -1,18 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" width="80" src="https://avatars.githubusercontent.com/u/91677148?v=4" />
-    <HelloWorld msg="Welcome to Hacktoberfest 2023 x KCEspot Community" />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
 
---
+++ gym-frontend/src/pages/PageNotFound.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="errorImg">
+        <img src="https://www.artzstudio.com/content/images/wordpress/2020/05/404-error-not-found-page-lost.png" alt="page not found">
+    </div>
+</template>
+
+<style scoped>
+  img{
+    width: 400px;
+    height: 400px;
+    /* position: absolute; */
+  }
+</style>
