08c5ae2dc4aa343a0f8a44694a2031ab5fa5ffa2
vigneshrml <vickysathy22@gmail.com>
2022-10-04 12:33:50 +0530

    Store init

--- gym-frontend/src/main.js
+++ gym-frontend/src/main.js
@@ -1,11 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-import router from "./router/index"
+import store from "./store/index.js";
+import router from "./router/index";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
+  store,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
